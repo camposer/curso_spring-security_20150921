e4de49e39c11de0c9672dd30e9af9db06ae9fa51
Rodolfo Campos <camposer@gmail.com>
2015-09-21 17:01:48 +0200

    Model de artículo

---
+++ blog/src/main/java/model/Articulo.java
@@ -0,0 +1,68 @@
+package model;
+
+import java.io.Serializable;
+import javax.persistence.*;
+import java.util.Date;
+
+
+/**
+ * The persistent class for the ARTICULO database table.
+ * 
+ */
+@Entity
+@Table(name="ARTICULO")
+@NamedQuery(name="Articulo.findAll", query="SELECT a FROM Articulo a")
+public class Articulo implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(unique=true, nullable=false)
+	private Integer id;
+
+	@Temporal(TemporalType.DATE)
+	@Column(name="CREATED_AT", nullable=false)
+	private Date createdAt;
+
+	@Column(nullable=false, length=500)
+	private String texto;
+
+	@Column(nullable=false, length=250)
+	private String titulo;
+
+	public Articulo() {
+	}
+
+	public Integer getId() {
+		return this.id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public Date getCreatedAt() {
+		return this.createdAt;
+	}
+
+	public void setCreatedAt(Date createdAt) {
+		this.createdAt = createdAt;
+	}
+
+	public String getTexto() {
+		return this.texto;
+	}
+
+	public void setTexto(String texto) {
+		this.texto = texto;
+	}
+
+	public String getTitulo() {
+		return this.titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+}
