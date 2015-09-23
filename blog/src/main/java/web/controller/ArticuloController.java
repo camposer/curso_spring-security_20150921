0032e316aebcf41fb4120814e57d44f228e54267
Rodolfo Campos <camposer@gmail.com>
2015-09-23 19:00:41 +0200

    Configuración cambiada a XML (falla al autenticar\!)

--- blog/src/main/java/web/controller/ArticuloController.java
+++ blog/src/main/java/web/controller/ArticuloController.java
@@ -1,9 +1,7 @@
 package web.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
