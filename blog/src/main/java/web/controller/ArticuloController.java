b0821e8b412d4fe4830abb578bf5895bfeb82022
Rodolfo Campos <camposer@gmail.com>
2015-09-23 18:22:11 +0200

    Imprimiendo roles en ArticuloController

--- blog/src/main/java/web/controller/ArticuloController.java
+++ blog/src/main/java/web/controller/ArticuloController.java
@@ -1,6 +1,10 @@
 package web.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,6 +20,10 @@ public class ArticuloController {
 	// http://localhost:8080/blog/articulos.do
 	@RequestMapping("")
 	public String index(Model model) {
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+
+		System.out.println(auth.getAuthorities());
+		
 		model.addAttribute("articulos", articuloService.obtenerArticulos());
 		return "/WEB-INF/jsp/articulo/index.jsp";
 	}
